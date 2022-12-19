3b654a3821a0ba07bc36f5939d613b358ce998ba
Uranuscsz <1092639340@qq.com>
2022-12-19 18:06:54 +0800

    add vite_native

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+// vite.config.ts
+export default defineConfig(()=>{
+  return {
+      plugins: [vue()],
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            nested: resolve(__dirname, 'src/nested/index.html'),
+          },
+        },
+      },
+  }
 })
